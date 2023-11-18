683ff7c14146f41daefac859216d45d98390ee2c
maitri-97 <mait.ch1997@gmail.com>
2023-11-18 21:43:27 +0530

    Updates

--- src/services/general.ts
+++
@@ -1,8 +0,0 @@
-export const getStorageData = (key: string) => {
-   let storageData =  localStorage.getItem(key) || '';
-   return storageData;
-}
-
-export const setStorageData = (key:string, value:string) => {
-    localStorage.setItem(key, value);
-}
